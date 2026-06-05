a8f7758fd5d7bfebaa308820e8fd3ac5fb04f984
Gunasheelan24 <gunasheelan16@gmail.com>
2026-06-05 22:26:59 +0530

    React-Router: completed layout routing & nested and protected routes

--- src/features/blogs/Index.tsx
+++ src/features/blogs/Index.tsx
@@ -1,9 +1,34 @@
-import React from "react";
+import React, { useEffect } from "react";
+import {
+  //  useParams,
+  useSearchParams,
+} from "react-router-dom";
 
 const Blog: React.FC = () => {
+  // const paramsObject = useParams();
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  useEffect(() => {
+    // console.log(paramsObject.user);
+    console.log(searchParams.get("page"));
+  });
+
+  // JSX Return
   return (
-    <main className="h-screen w-screen bg-slate-500 flex items-center justify-center text-white">
-      <h1>Blogs</h1>
+    <main className="h-screen w-screen bg-slate-500 text-white flex items-center justify-center">
+      <section>
+        <h1 className="block">Blogs</h1>
+        <button
+          onClick={() =>
+            setSearchParams({
+              page: "2",
+            })
+          }
+          className="block mt-5 border p-2 rounded-xl bg-black border-transparent cursor-pointer"
+        >
+          Change Page
+        </button>
+      </section>
     </main>
   );
 };
